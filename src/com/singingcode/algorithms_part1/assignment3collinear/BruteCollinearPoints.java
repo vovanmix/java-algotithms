4d478a5b03f2989fd61d859bce57e10f4a84704d
Vladimir Mikhaylovskiy <vlad@indiegogo.com>
2017-06-07 13:56:56 -0700

    collinear WIP

---
+++ src/com/singingcode/algorithms_part1/assignment3collinear/BruteCollinearPoints.java
@@ -0,0 +1,12 @@
+package com.singingcode.algorithms_part1.assignment3collinear;
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+    }
+
+    public int numberOfSegments() {
+    }
+
+    public LineSegment[] segments() {
+    }
+}
